14e719384c1c46a22dee82e8ed9dd470b47ed289
kunal <kunalb8607@gmail.com>
2015-07-30 20:26:31 +0530

    added task to restart sercer

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,26 +10,14 @@ set :scm, :git
 set :use_sudo, false
 set :deploy_to, "/home/ubuntu/www/demo_app"
 
-desc "check production task"
-task :check_production do
-
-if stage.to_s == "production"
-puts " \n Are you REALLY sure you want to deploy to production?"
-puts " \n Enter the password to continue\n "
-password = STDIN.gets[0..7] rescue nil
-if password != 'mypasswd'
-puts "\n !!! WRONG PASSWORD !!!"
-exit
-end
 
 end
 
-end
-
-task :bundle_install_karo do
+task :do_bundle_install do
 
   run  'cd /home/ubuntu/www/demo_app/current && bundle install && rake db:migrate'
+  run 'echo restarting apache'
+  run 'sudo /etc/init.d/apache2 restart'
 end
 
-before "deploy", "check_production"
-after 'deploy', 'bundle_install_karo'
+after 'deploy', 'do_bundle_install'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150729133943) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
